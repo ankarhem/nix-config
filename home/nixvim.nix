ecc5b9f31c80fb856c11e1c5b4195d99734edadc
Jakob Ankarhem <jakob@ankarhem.dev>
2024-05-31 23:27:32 +0200

    add telescope keybinds

--- home/nixvim.nix
+++ home/nixvim.nix
@@ -28,11 +28,34 @@
 
     plugins = {
       lightline.enable = true;
-      telescope.enable = true;
       oil.enable = true;
       treesitter.enable = true;
       luasnip.enable = true;
       flash.enable = true;
+      which-key.enable = true;
+    };
+
+    plugins.telescope = {
+      enable = true;
+
+      keymaps = {
+        "<leader>fp" = {
+          action = "git_files";
+          options.desc = "Telescope Find Project";
+        };
+        "<leader>ff" = {
+          action = "find_files";
+          options.desc = "Telescope Find Files";
+        };
+        "<leader>fg" = {
+          action = "live_grep";
+          options.desc = "Telescope Find Grep";
+        };
+        "<leader>fb" = {
+          action = "buffers";
+          options.desc = "Telescope Find Buffers";
+        };
+      };
     };
 
     plugins.lsp = {
