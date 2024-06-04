ee672e237ff4d38c66c223fc66af80175bc65aec
Jakob Ankarhem <jakob@ankarhem.dev>
2024-06-04 19:50:23 +0200

    add fish plugins

--- home/default.nix
+++ home/default.nix
@@ -63,7 +63,19 @@
     '';
     # Reorders stuff so that nix can override system binaries
     loginShellInit = "fish_add_path --move --prepend --path $HOME/.nix-profile/bin /run/wrappers/bin /etc/profiles/per-user/$USER/bin /nix/var/nix/profiles/default/bin /run/current-system/sw/bin";
+
+    plugins = with pkgs.fishPlugins; [
+      {
+        name = "fzf-fish";
+        src = fzf-fish.src;
+      }
+      {
+        name = "git-abbr";
+        src = git-abbr.src;
+      }
+    ];
   };
+  programs.fzf.enable = true;
 
   programs.zoxide = {
     enable = true;
