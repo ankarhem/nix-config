719d275f816de507ed779960b513dd687530b964
Jakob Ankarhem <jakob@ankarhem.dev>
2024-06-05 16:01:33 +0200

    add a bunch of lsps

--- home/nixvim/plugins/lsp.nix
+++ home/nixvim/plugins/lsp.nix
@@ -2,7 +2,19 @@
   enable = true;
   servers = {
     tsserver.enable = true;
+    html.enable = true;
+    cssls.enable = true;
+    jsonls.enable = true;
+    svelte.enable = true;
+    tailwindcss.enable = true;
+
+    csharp-ls.enable = true;
+    omnisharp.enable = true;
+
     lua-ls.enable = true;
+    nginx-language-server.enable = true;
+    bashls.enable = true;
+
     rust-analyzer = {
       enable = true;
       installRustc = false;
