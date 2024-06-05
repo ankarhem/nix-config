29e9ad25d96ecb9cbdce6d20f72e06f2b69405f2
Jakob Ankarhem <jakob@ankarhem.dev>
2024-06-05 19:14:42 +0200

    dont write ds_stores

--- hosts/darwin/settings.nix
+++ hosts/darwin/settings.nix
@@ -83,5 +83,11 @@
     "com.jetbrains.rustrover-EAP" = {
       ApplePressAndHoldEnabled = false;
     };
+
+    "com.apple.desktopservices" = {
+      # Avoid creating .DS_Store files on network or USB volumes
+      DSDontWriteNetworkStores = true;
+      DSDontWriteUSBStores = true;
+    };
   };
 }
