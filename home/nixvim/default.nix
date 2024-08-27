714cc87f2fcfae7148464079faecff75dfef0329
Jakob Ankarhem <jakob@ankarhem.dev>
2024-08-27 22:29:57 +0200

    add some nixvim plugins

--- home/nixvim/default.nix
+++ home/nixvim/default.nix
@@ -5,13 +5,23 @@
     keymaps = import ./keymaps.nix;
 
     plugins = {
+      direnv.enable = true;
       lightline.enable = true;
       oil.enable = true;
       luasnip.enable = true;
       which-key.enable = true;
-
       flash = import ./plugins/flash.nix;
       telescope = import ./plugins/telescope.nix;
+      gitsigns = {
+        enable = true;
+
+        settings = {
+          current_line_blame = true;
+        };
+      };
+
+      chatgpt.enable = true;
+      copilot-chat.enable = true;
       lsp = import ./plugins/lsp.nix;
       treesitter = import ./plugins/treesitter.nix;
     };
