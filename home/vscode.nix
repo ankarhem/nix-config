ed6ec3e1f981adb9c4d6471436f38e3df939fd11
Jakob Ankarhem <jakob@ankarhem.dev>
2024-10-04 14:26:23 +0200

    add vue vscode ext

--- home/vscode.nix
+++ home/vscode.nix
@@ -14,6 +14,7 @@
       # Javascript
       svelte.svelte-vscode
       angular.ng-template
+      vue.volar
       ## Angular
       bradlc.vscode-tailwindcss
       (pkgs.vscode-utils.buildVscodeMarketplaceExtension rec {
