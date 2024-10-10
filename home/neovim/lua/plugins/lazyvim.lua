aa0d35dd1e99daa690946896f8e96ad37a0879c0
Jakob Ankarhem <jakob@ankarhem.dev>
2024-10-10 14:37:43 +0200

    use catppuccin-latte theme

---
+++ home/neovim/lua/plugins/lazyvim.lua
@@ -0,0 +1,30 @@
+return {
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "catppuccin-latte",
+		},
+	},
+
+	-- Languages
+	{ import = "lazyvim.plugins.extras.vscode" },
+	{ import = "lazyvim.plugins.extras.lang.angular" },
+	{ import = "lazyvim.plugins.extras.lang.git" },
+	{ import = "lazyvim.plugins.extras.lang.go" },
+	{ import = "lazyvim.plugins.extras.lang.json" },
+	-- { import = "lazyvim.plugins.extras.lang.markdown" },
+	{ import = "lazyvim.plugins.extras.lang.nix" },
+	{ import = "lazyvim.plugins.extras.lang.omnisharp" },
+	{ import = "lazyvim.plugins.extras.lang.rust" },
+	{ import = "lazyvim.plugins.extras.lang.svelte" },
+	{ import = "lazyvim.plugins.extras.lang.tailwind" },
+	{ import = "lazyvim.plugins.extras.lang.toml" },
+	{ import = "lazyvim.plugins.extras.lang.typescript" },
+	{ import = "lazyvim.plugins.extras.lang.yaml" },
+	-- Linting
+	{ import = "lazyvim.plugins.extras.linting.eslint" },
+	-- Lsp
+	{ import = "lazyvim.plugins.extras.lsp.none-ls" },
+	-- Test
+	{ import = "lazyvim.plugins.extras.test.core" },
+}
