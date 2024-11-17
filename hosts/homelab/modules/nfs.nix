19bca3b9fa0e8d5fdfd1a3df83715873d4ab67e7
Jakob Ankarhem <jakob@ankarhem.dev>
2024-11-17 13:12:47 +0100

    nfs working, v3

--- hosts/homelab/modules/nfs.nix
+++ hosts/homelab/modules/nfs.nix
@@ -3,13 +3,18 @@
   networking.hosts = {
     "192.168.1.163" = ["disketten.local"];
   };
+  boot.initrd = {
+    supportedFilesystems = [ "nfs" ];
+    kernelModules = [ "nfs" ];
+  };
 
-  # fileSystems."/mnt/DISKETTEN_media" = {
-  #   device = "disketten.local:/volume1/media";
-  #   fsType = "nfs";
-  #   options = [
-  #     "x-systemd.automount"
-  #     "noauto"
-  #   ];
-  # };
+  fileSystems."/mnt/DISKETTEN_media" = {
+    device = "disketten.local:/volume1/media";
+    fsType = "nfs";
+    options = [
+      "nfsvers=3"
+      "x-systemd.automount"
+      "noauto"
+    ];
+  };
 }
