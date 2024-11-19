9d82722f40cced3a1f6ca02e49bada0ac28229e7
Jakob Ankarhem <jakob@ankarhem.dev>
2024-11-19 13:45:34 +0100

    remove mb.lib on gc

--- hosts/mbp/nix.nix
+++ hosts/mbp/nix.nix
@@ -13,8 +13,8 @@
 
   # do garbage collection weekly to keep disk usage low
   nix.gc = {
-    automatic = lib.mkDefault true;
-    options = lib.mkDefault "--delete-older-than 7d";
+    automatic = true;
+    options = "--delete-older-than 7d";
   };
 
   nixpkgs.config.allowUnfree = true;
