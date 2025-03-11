699eade17829d22203ea4bc3c55d7906ddb54990
Jakob Ankarhem <jakob@ankarhem.dev>
2025-03-11 23:15:33 +0100

    simplify omada

--- hosts/homelab/apps/omada.nix
+++ hosts/homelab/apps/omada.nix
@@ -1,12 +1,14 @@
-{...}:let
+{lib,...}:let
+  toInt = lib.strings.toInt;
+
   ports = {
     http = "8088";
     https = "8043";
-    portal-http = "8088";
-    portal-https = "8843";
+    # portal-http = "8088";
+    # portal-https = "8843";
 
-    app-discovery = "27001";
-    discovery = "29810";
+    app-discovery = "27001"; # UDP
+    discovery = "29810"; # UDP
     manager-v1 = "29811";
     adopt-v1 = "29812";
     upgrade-v1 = "29813";
@@ -14,7 +16,16 @@
     transfer-v2 = "29815";
     rtty = "29816";
   };
+
+  udpPorts = [
+    ports.app-discovery
+    ports.discovery
+  ];
+  tcpPorts = builtins.filter (port: !builtins.elem port udpPorts) (builtins.attrValues ports);
 in {
+  networking.firewall.allowedTCPPorts = map toInt tcpPorts;
+  networking.firewall.allowedUDPPorts = map toInt udpPorts;
+
   services.nginx.virtualHosts."omada.internal.internetfeno.men" =  {
     forceSSL = true;
     useACMEHost = "internal.internetfeno.men";
@@ -28,8 +39,8 @@ in {
     environment = {
       "MANAGE_HTTP_PORT" = ports.http;
       "MANAGE_HTTPS_PORT" = ports.https;
-      "PORTAL_HTTP_PORT" = ports.portal-http;
-      "PORTAL_HTTPS_PORT" = ports.portal-https;
+      "PORTAL_HTTP_PORT" = ports.http;
+      "PORTAL_HTTPS_PORT" = ports.https;
       "PORT_APP_DISCOVERY" = ports.app-discovery;
       "PORT_DISCOVERY" = ports.discovery;
       "PORT_MANAGER_V1" = ports.manager-v1;
@@ -42,21 +53,8 @@ in {
       "SHOW_SERVER_LOGS" = "true";
       "TZ" = "Europe/Stockholm";
     };
-    ports = [
-      "${ports.http}:${ports.http}"
-      "${ports.https}:${ports.https}"
-      "${ports.portal-https}:${ports.portal-https}"
-
-      "${ports.app-discovery}:${ports.app-discovery}/udp"
-      "${ports.discovery}:${ports.discovery}/udp"
-
-      "${ports.manager-v1}:${ports.manager-v1}"
-      "${ports.adopt-v1}:${ports.adopt-v1}"
-      "${ports.upgrade-v1}:${ports.upgrade-v1}"
-      "${ports.manager-v2}:${ports.manager-v2}"
-      "${ports.transfer-v2}:${ports.transfer-v2}"
-      "${ports.rtty}:${ports.rtty}"
-    ];
+    ports = (builtins.map (port: "${port}:${port}") tcpPorts) ++
+      (builtins.map (port: "${port}:${port}/udp") udpPorts);
     volumes = [
       "/var/lib/omada/data:/opt/tplink/EAPController/data:rw"
       "/var/lib/omada/logs:/opt/tplink/EAPController/logs:rw"
