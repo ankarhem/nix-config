60f251550c738be57e90a78827828dbfd07ea44c
Jakob Ankarhem <jakob@ankarhem.dev>
2025-04-01 13:13:11 +0200

    update recyclarr

--- hosts/homelab/apps/arrs/recyclarr.nix
+++ hosts/homelab/apps/arrs/recyclarr.nix
@@ -20,10 +20,49 @@
       radarr.movies = {
         base_url = "https://radarr.internal.internetfeno.men";
         api_key._secret = "/run/credentials/recyclarr.service/radarr_api_key";
+
+        delete_old_custom_formats = true;
+        replace_existing_custom_formats = true;
+
+        media_naming = {
+          folder = "plex-tmdb";
+          movie = {
+            rename = true;
+            standard = "plex-tmdb";
+          };
+        };
+
+        include = [
+          # Comment out any of the following includes to disable them
+          { template = "radarr-quality-definition-movie"; }
+          { template = "radarr-quality-profile-hd-bluray-web"; }
+          { template = "radarr-custom-formats-hd-bluray-web"; }
+        ];
       };
       sonarr.tv = {
         base_url = "https://sonarr.internal.internetfeno.men";
         api_key._secret = "/run/credentials/recyclarr.service/sonarr_api_key";
+
+        delete_old_custom_formats = true;
+        replace_existing_custom_formats = true;
+
+        media_naming = {
+          series = "default";
+          season = "default";
+          episodes = {
+            rename = true;
+            standard = "default";
+            daily = "default";
+            anime = "default";
+          };
+        };
+
+        include = [
+          # Comment out any of the following includes to disable them
+          { template = "sonarr-quality-definition-series"; }
+          { template = "sonarr-v4-quality-profile-web-1080p"; }
+          { template = "sonarr-v4-custom-formats-web-1080p"; }
+        ];
       };
     };
   };
