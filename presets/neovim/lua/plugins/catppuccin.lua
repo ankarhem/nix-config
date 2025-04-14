dc78f083ae583fb1d384d03fbc3ce1f69ce01294
Jakob Ankarhem <jakob@ankarhem.dev>
2025-04-15 00:12:51 +0200

    use system theme for catppuccin

---
+++ presets/neovim/lua/plugins/catppuccin.lua
@@ -0,0 +1,13 @@
+return {
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		opts = {
+			flavour = "auto", -- latte, frappe, macchiato, mocha
+			background = { -- :h background
+				light = "latte",
+				dark = "frappe",
+			},
+		},
+	},
+}
