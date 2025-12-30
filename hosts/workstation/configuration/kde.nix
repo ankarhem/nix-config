b4dfdea79c1d6de86696f66b15ee18fbd19e543c
Jakob Ankarhem <jakob@ankarhem.dev>
2025-12-30 08:16:07 +0100

    enable wayland

--- hosts/workstation/configuration/kde.nix
+++ hosts/workstation/configuration/kde.nix
@@ -3,7 +3,11 @@
   # You can disable this if you're only using the Wayland session.
   services.xserver.enable = true;
   # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
+  services.displayManager.sddm = {
+    enable = true;
+    wayland.enable = true;
+    autoNumlock = true;
+  };
   services.desktopManager.plasma6.enable = true;
   programs.kdeconnect.enable = true;
 
