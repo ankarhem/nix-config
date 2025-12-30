f2a3b5a8752baad6738e42bdc01cb36f61ac052a
Jakob Ankarhem <jakob@ankarhem.dev>
2025-12-30 11:42:43 +0100

    add comments for howto

--- hosts/workstation/configuration/norcevpn.nix
+++ hosts/workstation/configuration/norcevpn.nix
@@ -1,5 +1,10 @@
 { ... }:
 {
+
+  # Download profile from https://jetshop.openvpn.com/devices
+  # Import and connect to profile
+  # openvpn3 config-import --config ./norce.ovpn --name NorceVPN --persistent
+  # openvpn3 session-start --config NorceVPN
   programs.openvpn3.enable = true;
 
   # https://github.com/NixOS/nixpkgs/pull/396940
