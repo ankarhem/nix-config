69cde467997361cd8773fa819742e187555818d6
Jakob Ankarhem <jakob@ankarhem.dev>
2025-12-30 11:49:47 +0100

    add aliases

--- hosts/workstation/configuration/norcevpn.nix
+++ hosts/workstation/configuration/norcevpn.nix
@@ -5,7 +5,14 @@
   # Import and connect to profile
   # openvpn3 config-import --config ./norce.ovpn --name NorceVPN --persistent
   # openvpn3 session-start --config NorceVPN
+  # openvpn3 session-manage --config NorceVPN --disconnect
   programs.openvpn3.enable = true;
+
+  # Helper aliases
+  environment.shellAliases = {
+    norcevpn-up = "openvpn3 session-start --config NorceVPN";
+    norcevpn-down = "openvpn3 session-manage --config NorceVPN --disconnect";
+  };
 
   # https://github.com/NixOS/nixpkgs/pull/396940
   services.resolved.enable = true;
