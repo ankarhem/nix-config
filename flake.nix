abe25753ca321a499ed7da4519058d69d8a13c56
Jakob Ankarhem <jakob@ankarhem.dev>
2026-01-04 11:06:57 +0100

    use neochat from unstable

--- flake.nix
+++ flake.nix
@@ -105,6 +105,10 @@
         let
           nixpkgsConfig = {
             allowUnfree = true;
+            # temporary allow olm-3.2.16 as insecure package
+            permittedInsecurePackages = [
+              "olm-3.2.16"
+            ];
           };
         in
         {
--- hosts/workstation/configuration/neochat.nix
+++ hosts/workstation/configuration/neochat.nix
@@ -1,11 +1,6 @@
-{ pkgs, ... }:
+{ pkgs, pkgs-unstable, ... }:
 {
-  # temporary allow olm-3.2.16 as insecure package
-  nixpkgs.config.permittedInsecurePackages = [
-    "olm-3.2.16"
-  ];
-
   environment.systemPackages = [
-    pkgs.kdePackages.neochat
+    pkgs-unstable.kdePackages.neochat
   ];
 }
