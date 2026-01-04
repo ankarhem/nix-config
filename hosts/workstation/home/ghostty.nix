4e20384b0c31dbb2d3068b7498a1890636a7685c
Jakob Ankarhem <jakob@ankarhem.dev>
2026-01-04 13:56:24 +0100

    dont use apple font in ghostty

--- hosts/workstation/home/ghostty.nix
+++ hosts/workstation/home/ghostty.nix
@@ -4,7 +4,6 @@ _: {
     enableFishIntegration = true;
 
     settings = {
-      font-family = "SFProDisplay Nerd Font";
       font-size = 12;
       theme = "dark:Catppuccin Frappe,light:Catppuccin Latte";
       shell-integration-features = "ssh-terminfo,ssh-env";
