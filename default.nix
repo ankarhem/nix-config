75395cec3bd7f0f11dc20e6edd90c58c71693c92
Jakob Ankarhem <jakob@ankarhem.dev>
2026-01-05 22:59:40 +0100

    move devShells

--- default.nix
+++ default.nix
@@ -36,4 +36,28 @@ inputs.flake-parts.lib.mkFlake { inherit inputs; } [
         };
     }
   )
+
+  # Devshells
+  {
+    perSystem =
+      { pkgs }:
+      let
+        pre-commit-hooks = inputs.git-hooks.lib.${system}.run {
+          src = ./.;
+
+          hooks = {
+            ripsecrets.enable = true;
+            nixfmt-rfc-style.enable = true;
+          };
+        };
+      in
+      {
+        default = pkgs.mkShell {
+          buildInputs = [ pre-commit-hooks.enabledPackages ];
+          shellHook = ''
+            ${pre-commit-hooks.shellHook}
+          '';
+        };
+      };
+  }
 ]
