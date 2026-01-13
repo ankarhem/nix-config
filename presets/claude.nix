5444b463e52b8fdf09a4a4f71298fde8ef1900ca
Jakob Ankarhem <jakob@ankarhem.dev>
2026-01-13 14:02:34 +0100

    enable plugins

--- presets/claude.nix
+++ presets/claude.nix
@@ -94,6 +94,13 @@ in
         env = {
           API_TIMEOUT_MS = "3000000";
         };
+        enabledPlugins = lib.genAttrs [
+          "commit-commands@claude-plugins-official"
+          "csharp-lsp@claude-plugins-official"
+          "ralph-wiggum@claude-plugins-official" # This is not it; https://www.youtube.com/watch?v=O2bBWDoxO4s
+          "rust-analyzer-lsp@claude-plugins-official"
+          "typescript-lsp@claude-plugins-official"
+        ] (_: true);
       };
       mcpServers = {
         playwrite = {
