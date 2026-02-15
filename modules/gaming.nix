ac61e35c659b71f4c53165039c0e13fe69ef8989
Jakob Ankarhem <jakob@ankarhem.dev>
2026-02-15 10:45:29 +0100

    add gaming module

---
+++ modules/gaming.nix
@@ -0,0 +1,67 @@
+{ inputs, ... }:
+{
+  flake.modules.nixos.gaming =
+    { pkgs, ... }:
+    {
+      home-manager.sharedModules = [
+        inputs.self.modules.homeManager.gaming
+      ];
+
+      programs.steam = {
+        enable = true;
+        remotePlay.openFirewall = true;
+        protontricks.enable = true;
+      };
+      # https://nixos.wiki/wiki/Gamemode#Steam
+      programs.gamemode.enable = true;
+      programs.gamescope.enable = true;
+
+      environment.systemPackages = with pkgs; [
+        lutris
+        protonup-qt # Manage Proton versions
+      ];
+    };
+
+  flake.modules.homeManager.gaming = {
+    programs.mangohud = {
+      enable = true;
+      enableSessionWide = false;
+
+      settings = {
+        fps = true;
+        show_fps_limit = true;
+        frametime = true;
+        frame_timing = 1;
+        present_mode = true;
+        histogram = true;
+        ram = true;
+        fps_limit = [
+          240
+          120
+          0
+        ];
+        gl_vsync = -1;
+        vsync = 1;
+
+        cpu_stats = true;
+        cpu_temp = true;
+        cpu_power = true;
+        cpu_text = "CPU";
+        cpu_mhz = true;
+
+        throttling_status = true;
+        gpu_stats = true;
+        gpu_temp = true;
+        gpu_core_clock = true;
+        gpu_mem_clock = true;
+        gpu_power = true;
+        gpu_text = "GPU";
+        vram = true;
+
+        position = "top-left";
+        font_size = 32;
+        round_corners = 8;
+      };
+    };
+  };
+}
