1d47e35e613f34ec0d975d6171be7c956408eaaa
Jakob Ankarhem <jakob@ankarhem.dev>
2026-02-15 16:48:54 +0100

    fix homelab declaration

--- modules/hosts/homelab/flake-parts.nix
+++ modules/hosts/homelab/flake-parts.nix
@@ -1,4 +1,4 @@
 { inputs, ... }:
 {
-  flake.nixosConfigurations.homelab = inputs.self.factory.nixos "x86_64-linux" "homelab";
+  flake.nixosConfigurations = inputs.self.factory.nixos "x86_64-linux" "homelab";
 }
