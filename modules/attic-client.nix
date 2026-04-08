6d533f418cc36aa81c3a3f9b9e4201da0f5ec85b
Jakob Ankarhem <jakob@ankarhem.dev>
2026-04-08 16:16:18 +0200

    add watch-store daemon

--- modules/attic-client.nix
+++ modules/attic-client.nix
@@ -1,4 +1,9 @@
 let
+  cacheServerName = "homelab";
+  cacheName = "main";
+  cacheUrl = "https://attic.internetfeno.men";
+  cachePublicKey = "9AeGSCEdFACRMznpA8b1LZtZ6cLkpdeTQ0Hqa0RmQAA="; # pragma: allowlist secret
+
   sharedModule =
     { pkgs, ... }:
     {
@@ -7,21 +12,84 @@ let
       ];
       nix.settings = {
         substituters = [
-          "https://attic.internetfeno.men/main"
+          "${cacheUrl}/${cacheName}"
         ];
 
         trusted-public-keys = [
-          "main:9AeGSCEdFACRMznpA8b1LZtZ6cLkpdeTQ0Hqa0RmQAA="
+          "${cacheName}:${cachePublicKey}"
         ];
       };
+
+      sops.secrets."attic/client_token" = { };
     };
 
 in
 {
-  flake.modules.nixos.attic-client = {
-    imports = [ sharedModule ];
-  };
-  flake.modules.darwin.attic-client = {
-    imports = [ sharedModule ];
-  };
+  flake.modules.nixos.attic-client =
+    {
+      config,
+      lib,
+      pkgs,
+      ...
+    }:
+    {
+      imports = [ sharedModule ];
+
+      systemd.services.attic-watch-store = {
+        description = "Watch the Nix store and push new paths to Attic";
+        after = [ "network-online.target" ];
+        wants = [ "network-online.target" ];
+        wantedBy = [ "multi-user.target" ];
+
+        serviceConfig = {
+          Type = "simple";
+          User = "root";
+          Restart = "always";
+          RestartSec = "5s";
+          ExecStart =
+            let
+              atticBin = "${pkgs.attic-client}/bin/attic";
+            in
+            ''
+              ${atticBin} login --set-default ${cacheServerName} ${cacheUrl} $(cat ${
+                config.sops.secrets."attic/client_token".path
+              }) || true
+              ${atticBin} cache create ${cacheName} || true
+              ${atticBin} cache info ${cacheName}
+              ${atticBin} watch-store ${cacheName}
+            '';
+        };
+      };
+    };
+  flake.modules.darwin.attic-client =
+    {
+      config,
+      lib,
+      pkgs,
+      ...
+    }:
+    {
+      imports = [ sharedModule ];
+
+      launchd.daemons.attic-watch-store = {
+        script =
+          let
+            atticBin = "${pkgs.attic-client}/bin/attic";
+          in
+          ''
+            ${atticBin} login --set-default ${cacheServerName} ${cacheUrl} $(cat ${
+              config.sops.secrets."attic/client_token".path
+            }) || true
+            ${atticBin} cache create ${cacheName} || true
+            ${atticBin} cache info ${cacheName}
+            ${atticBin} watch-store ${cacheName}
+          '';
+        serviceConfig = {
+          KeepAlive = true;
+          RunAtLoad = true;
+          StandardOutPath = "/var/log/attic-watch-store.log";
+          StandardErrorPath = "/var/log/attic-watch-store.log";
+        };
+      };
+    };
 }
