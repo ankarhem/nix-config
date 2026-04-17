f0256f086e62ee85cb00a1ddf13708f638549ae5
Jakob Ankarhem <jakob@ankarhem.dev>
2026-04-17 14:02:13 +0200

    update forgejo settings

--- modules/forgejo.nix
+++ modules/forgejo.nix
@@ -8,13 +8,40 @@ in
   flake.modules.nixos.forgejo =
     { config, ... }:
     {
+      sops.secrets = {
+        "smtp/username" = { };
+        "smtp/password" = { };
+      };
       services.forgejo = {
         enable = true;
         lfs.enable = true;
 
+        secrets.mailer.PASSWD = config.sops.secrets."smtp/password".path;
+        secrets.mailer.USER = config.sops.secrets."smtp/username".path;
         settings = {
-          DEFAULT.APP_NAME = "Ankarhem Git";
+          # service.DISABLE_REGISTRATION = true;
+          DEFAULT.APP_NAME = "forgejo";
+          log.LEVEL = "Debug";
+          federation.ENABLED = true;
+          ui = {
+            DEFAULT_THEME = "forgejo-dark";
+            SHOW_USER_EMAIL = true;
+          };
+          actions = {
+            ENABLED = false;
+            DEFAULT_ACTIONS_URL = "https://code.forgejo.org";
+          };
 
+          mailer = {
+            ENABLED = true;
+            FROM = "Forgejo <admin@ankarhem.dev>";
+            PROTOCOL = "smtp+starttls";
+            SMTP_ADDR = "smtp.mail.me.com";
+            SMTP_PORT = 587;
+          };
+          security = {
+            LOGIN_REMEMBER_DAYS = 365;
+          };
           server = {
             HTTP_PORT = port;
             HTTP_ADDR = "127.0.0.1";
@@ -29,6 +56,24 @@ in
             COOKIE_SECURE = true;
           };
         };
+      };
+
+      services.fail2ban.jails.forgejo = {
+        settings = {
+          filter = "forgejo";
+          action = "iptables-allports";
+          mode = "aggressive";
+          maxretry = 3;
+          findtime = 3600;
+          bantime = 900;
+        };
+      };
+      environment.etc = {
+        "fail2ban/filter.d/forgejo.conf".text = ''
+          [Definition]
+          failregex = ^.*(Failed authentication attempt|invalid credentials|Attempted access of unknown user).* from <HOST>$
+          journalmatch = _SYSTEMD_UNIT=forgejo.service
+        '';
       };
 
       networking.firewall.allowedTCPPorts = [ sshPort ];
