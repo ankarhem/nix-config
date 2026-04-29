9b0d5bc5c5138ab2ef9cd71beda16236874e34a3
Jakob Ankarhem <jakob@ankarhem.dev>
2026-04-29 19:51:03 +0200

    add some extensions

--- modules/zed.nix
+++ modules/zed.nix
@@ -3,8 +3,23 @@
     programs.zed-editor = {
       enable = true;
       extensions = [
+        "angular"
+        "csharp"
+        "just"
         "nix"
+        "opencode"
+        "svelte"
       ];
+      userSettings = {
+        agent = {
+          always_allow_tool_actions = true;
+          model_parameters = [ ];
+        };
+        theme = "One Dark";
+        session.trust_all_worktrees = true;
+        vim_mode = true;
+        base_keymap = "VSCode";
+      };
     };
   };
 }
