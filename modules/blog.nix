f7aa57e8dc6a9e5c77028d46bce8f49150521165
Jakob Ankarhem <jakob@ankarhem.dev>
2026-06-08 12:22:26 +0200

    use tmpfiles

--- modules/blog.nix
+++ modules/blog.nix
@@ -3,24 +3,10 @@
   flake.modules.nixos.blog =
     { pkgs, ... }:
     {
-      systemd.services.link-blog-wwwroot = {
-        after = [ "local-fs.target" ];
-        before = [ "nginx.service" ];
-        wantedBy = [ "multi-user.target" ];
-        serviceConfig = {
-          Type = "oneshot";
-          RemainAfterExit = true;
-        };
-        script = ''
-          # Cold-start seed: only link the blog build if nothing is serving yet.
-          # Once deploy-rs (or a previous seed) has pointed this at a valid
-          # directory, the guard is true and we leave the deployed build alone.
-          if [ ! -d /var/www/ankarhem.dev ]; then
-              mkdir -p /var/www
-              ln -sfn "${inputs.blog.packages.${pkgs.hostPlatform.system}.blog}/public" /var/www/ankarhem.dev
-          fi
-        '';
-      };
+      systemd.tmpfiles.rules = [
+        "d /var/www 0755 root root - -"
+        "L /var/www/ankarhem.dev - - - - ${inputs.blog.packages.${pkgs.hostPlatform.system}.blog}/public"
+      ];
 
       services.nginx.virtualHosts."ankarhem.dev" = {
         forceSSL = true;
