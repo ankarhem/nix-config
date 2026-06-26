9fdac60b0aa66d6a34a5b197ebd1f81f42b00aad
Jakob Ankarhem <jakob@ankarhem.dev>
2026-06-26 02:13:34 +0200

    update tangled

--- flake.nix
+++ flake.nix
@@ -45,7 +45,7 @@
     sops-nix.url = "github:Mic92/sops-nix";
     tangled.inputs.flake-compat.url = "github:edolstra/flake-compat";
     tangled.inputs.nixpkgs.follows = "nixpkgs";
-    tangled.url = "git+https://tangled.org/tangled.org/core";
+    tangled.url = "git+https://tangled.org/@tangled.org/core";
     temporalio-skill.flake = false;
     temporalio-skill.url = "github:temporalio/skill-temporal-developer";
     truesight.inputs.nixpkgs.follows = "nixpkgs";
