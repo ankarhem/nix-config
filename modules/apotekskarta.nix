403ba2c8baada67025eb854f80c51291602fba74
Jakob Ankarhem <jakob@ankarhem.dev>
2026-08-14 13:25:39 +0200

    add geoapify key

--- modules/apotekskarta.nix
+++ modules/apotekskarta.nix
@@ -1,30 +1,39 @@
 {
-  flake.modules.nixos.apotekskarta = {
-    systemd.services.apotekskarta =
-      let
-        profile = "/nix/var/nix/profiles/per-user/root/apotekskarta";
-        binary = "${profile}/bin/apotekskarta";
-      in
-      {
-        wantedBy = [ "multi-user.target" ];
-        after = [ "network.target" ];
-        unitConfig.ConditionPathExists = binary;
-        serviceConfig = {
-          ExecStart = binary;
-          WorkingDirectory = "/var/lib/apotekskarta";
-          StateDirectory = "apotekskarta";
-          Restart = "always";
-          RestartSec = "5s";
-        };
-        environment = {
-          PORT = "7035";
-        };
-      };
+  flake.modules.nixos.apotekskarta =
+    { config, ... }:
+    {
+      sops.secrets."geoapify_key" = { };
 
-    services.nginx.virtualHosts."apotekskarta.internetfeno.men" = {
-      forceSSL = true;
-      useACMEHost = "internetfeno.men";
-      locations."/".proxyPass = "http://127.0.0.1:7035";
+      sops.templates."apotekskarta.env".content = ''
+        GEOAPIFY_API_KEY=${config.sops.placeholder."geoapify_key"}
+      '';
+
+      systemd.services.apotekskarta =
+        let
+          profile = "/nix/var/nix/profiles/per-user/root/apotekskarta";
+          binary = "${profile}/bin/apotekskarta";
+        in
+        {
+          wantedBy = [ "multi-user.target" ];
+          after = [ "network.target" ];
+          unitConfig.ConditionPathExists = binary;
+          serviceConfig = {
+            ExecStart = binary;
+            WorkingDirectory = "/var/lib/apotekskarta";
+            StateDirectory = "apotekskarta";
+            Restart = "always";
+            RestartSec = "5s";
+            EnvironmentFile = config.sops.templates."apotekskarta.env".path;
+          };
+          environment = {
+            PORT = "7035";
+          };
+        };
+
+      services.nginx.virtualHosts."apotekskarta.internetfeno.men" = {
+        forceSSL = true;
+        useACMEHost = "internetfeno.men";
+        locations."/".proxyPass = "http://127.0.0.1:7035";
+      };
     };
-  };
 }
